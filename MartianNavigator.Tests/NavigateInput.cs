5fc84d7747f58ab1061921e917982349a3ebe8bb
Mamed Mamedov <garrosh744@gmail.com>
2023-03-16 16:45:54 +0400

    Add navigate tests  with sample input from technical task

---
+++ MartianNavigator.Tests/NavigateInput.cs
@@ -0,0 +1,26 @@
+﻿using MartianNavigator.Enums;
+using System.Collections.Generic;
+
+namespace MartianNavigator.Tests
+{
+    public class NavigateInput
+    {
+        public int MaxX { get; set; }
+        public int MaxY { get; set; }
+
+        public IList<RobotInfo> RobotInfos { get; set; }
+    }
+
+    public class RobotInfo
+    {
+        public int InitialX { get; set; }
+        public int InitialY { get; set; }
+        public OrientationEnum InitialOrientation { get; set; }
+        public IEnumerable<CommandEnum> Commands { get; set; }
+
+        public int ExpectedX { get; set; }
+        public int ExpectedY { get; set; }
+        public OrientationEnum ExpectedOrientation { get; set; }
+        public RobotStatusEnum ExpectedRobotStatus { get; set; }
+    }
+}
